67ced7c94313255be4ea8deffb383f77a63e5dce
Pedro Campos <pedro.campos@radixeng.com.br>
2019-12-04 13:46:46 -0300

    PI Agent Base Windows Service

---
+++ PIAgent/PIAgent/PIAgent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PIAgent.ELEMENT
+{
+    public partial class PIAgent : ServiceBase
+    {
+        public PIAgent()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart.");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
